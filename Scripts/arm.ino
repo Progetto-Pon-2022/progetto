3ed6ac73b6886e6164df511d5353fbeca6413a8c
LeonardoRostagno <71812656+LeonardoRostagno@users.noreply.github.com>
2022-05-25 13:12:37 +0200

    Add files via upload

--- Scripts/arm.ino
+++ Scripts/arm.ino
@@ -1,84 +1,93 @@
 #include <Servo.h>
 
-int dist = 25; //DISTANZA MINIMA IN MM
+int dist = 25;
 
-int pos = 0;
-int posAttuale = 0;
-int valPrec = 0;
-int vLetto = 0;
-int time = 0;
-bool acceso = true;
+bool oggetto = true;
+
 Servo motore1;
 Servo motore2;
+Servo motore3;
 
 unsigned long int tempo_pulsante = 0;
-const int SONAR_TRIGGER_PIN = 6;
-const int SONAR_ECHO_PIN = 7;
-
-
-// Variabile che gestisce il numero associato allo stato
-int stato = 0;
+const int SONAR_TRIGGER_PIN = 8;
+const int SONAR_ECHO_PIN = 9;
 
 unsigned int measure_distance()
 {
  digitalWrite(SONAR_TRIGGER_PIN, HIGH);
- 
-//Tenendo alto per 10 microsecondi il trigger il sensore invia un impulso sonoro
-//e resta in attesa che ritorni dopo aver rimbalzato sull'ostacolo
 
  delayMicroseconds(10);
  digitalWrite(SONAR_TRIGGER_PIN, LOW);
  unsigned long durata_impulso = pulseIn (SONAR_ECHO_PIN, HIGH);
 
-// pulseln(,) legge quando il pin va al livello richiesto (nel nostro caso a livello alto)
-// e ci restituisce il tempo in ms di permanenza a quel livello
-
  delay(50);
  return( (unsigned int) (durata_impulso / 58) );
-// La funzione ci restituisce la distanza in cm (µs/58=cm)
 }
 
-// SETUP
 void setup()
 {
   Serial.begin(9600);
-  motore1.attach(9);
-  motore2.attach(10);
+  
+  motore1.attach(5);
+  motore2.attach(3);
+  motore3.attach(11);
+  
   pinMode(SONAR_TRIGGER_PIN, OUTPUT);
   pinMode(SONAR_ECHO_PIN, INPUT);
 }
 
 
-// LOOP
 void loop(){
-  morsa();
-}
-
-void morsa(){
   unsigned int distanza = measure_distance();
-   Serial.println(distanza);
-  if (distanza < dist){
-    posAttuale = 180;
-    Serial.println("si");
-    for (int i=0; i<180;i=i+5){
-      
-      motore1.write(i);
-      delay(50);
-    }
-    braccio();
-  }else{
-    Serial.println("no");
-    posAttuale = 0;
-    motore1.write(posAttuale);
-    delay(1000);
-    motore2.write(posAttuale);
-  }
+  Serial.print("distanza: ");
+  Serial.println(distanza);
+   
+  if (distanza < dist){ // se ho un oggetto vicino
+    Serial.println("Oggetto Trovato");
+    
+    Serial.print("Chiudo pinza - ");
+    motore1.write(50);
+    delay(2000);
+    
+    Serial.println(" Alzo Braccio");
+    braccio_su();
+    delay(2000);
+    
+    Serial.print("Apro pinza - ");
+    motore1.write(180);
+    delay(2000);
+    
+    Serial.println(" Abbasso Braccio");
+    braccio_giu();
+    delay(2000);
+    
+  }else{ // se non ho un oggetto vicino
+    Serial.println("Nessun oggetto");
+    
+    Serial.print("Pinza aperta - ");
+    motore1.write(180);
+    delay(2000);
   
+    Serial.println(" Braccio giu");
+    motore2.write(0);
+    motore3.write(95);
+    delay(2000);
+  }
+  Serial.println("");
 }
 
-void braccio(){
-  for (int i=0; i<95;i=i+2){
+void braccio_su(){
+  for (int i = 0; i < 95; i = i + 2){
     motore2.write(i);
+    motore3.write(95-i);
+    delay(100);
+  }
+}
+
+void braccio_giu() {
+  for (int i = 0; i < 95; i = i + 2){
+    motore3.write(i);
+    motore2.write(95-i);
     delay(100);
   }
 }
